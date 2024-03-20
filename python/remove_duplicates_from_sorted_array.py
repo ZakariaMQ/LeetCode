4aadd6dee0e800c983d70656f16d785afec36b62
zakariaMQ <zm554765@gmail.com>
2024-03-20 11:01:47 +0000

    added "remove_duplicates_from_sorted_array.py"

---
+++ python/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        unique_count = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[unique_count] = nums[i]
+                unique_count += 1
+        
+        return unique_count
