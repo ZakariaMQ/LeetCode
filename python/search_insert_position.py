7951888dbc7cd0ba5dca29d35778d63f11ce429d
zakariaMQ <zm554765@gmail.com>
2024-03-25 09:00:14 +0000

    added "search_insert_position.py"

--- python/search_insert_position.py
+++ python/search_insert_position.py
@@ -1,24 +1,3 @@
-# class Solution:
-#     def searchInsert(self, nums: list[int], target: int) -> int:
-#         if target == 0:
-#             return 0
-#         elif target == 1:
-#             return 1
-        
-#         j = 0
-
-
-#         for i in range(len(nums)-1):
-#             if nums[i] == target:
-#                 return i
-            
-#             elif nums[i+1]  > target:
-#                 print("elif")
-#                 return i+1
-#             j += 1
-
-#         return j if target in nums else j+1
-
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
         if target <= nums[0]:
@@ -29,7 +8,5 @@ class Solution:
         for i in range(lnums):
             if nums[i] >= target:
                 return i
+        
         return lnums
-
-res = Solution().searchInsert([-1,3,5,6], 70)
-print(res)
